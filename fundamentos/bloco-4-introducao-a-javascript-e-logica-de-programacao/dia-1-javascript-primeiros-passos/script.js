63bb3e844f71e9f0420afcd949f71779a66cd131
Omar Merljak <omerljak@gmail.com>
2021-07-28 20:13:42 -0300

    Resoluçao exercicios 1,2,3(duvida) e 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,52 @@
+//EXERCICIO 1
+
+let a = 44;
+let b = 27;
+let c = 107;
+
+//Adição 
+    if (a + b) {
+        console.log(a + b);
+    }
+//Subtração
+    if (a - b) {
+        console.log(a - b);
+    }
+//Multiplicação
+    if (a * b) {
+        console.log(a / b);
+    }
+//Módulo
+    if (a % b) {
+        console.log(a % b);
+    }
+
+//Exercicio 2
+
+    if (a > b) {
+     console.log("A é maior que B");
+ } else {
+     console.log("B é maior que A");
+ }
+
+ //Exercicio 3
+
+    if (a > b && c) {
+        console.log("A é o maior");
+    } else if (a < b && b > c) {
+        console.log("B é o maior");
+    } else {
+        console.log("C é o maior");
+    }
+// Resultado : A é maior que B //A é o mairo (porque)
+
+
+//Exercicio 4
+
+    if (a > 0) {
+        console.log("Positive");
+    } else if (a < 0) {
+        console.log("Negative");
+    } else {
+        console.log("ZERO");
+    }

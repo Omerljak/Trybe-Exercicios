b56122766fa49ac1ca79c52dcbe3f59a9298486e
Omar Merljak <omerljak@gmail.com>
2021-07-29 09:49:28 -0300

    Exercicios 5 e 7 solucionados

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -31,14 +31,13 @@ let c = 107;
 
  //Exercicio 3
 
-    if (a > b && c) {
+    if (a > b && a > c) {
         console.log("A é o maior");
-    } else if (a < b && b > c) {
+    } else if (b > c) {
         console.log("B é o maior");
     } else {
         console.log("C é o maior");
     }
-// Resultado : A é maior que B //A é o mairo (porque)
 
 
 //Exercicio 4
@@ -50,3 +49,43 @@ let c = 107;
     } else {
         console.log("ZERO");
     }
+
+//Exercicio 5 (Duvidas)
+
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+
+
+if (angulo1 + angulo2 + angulo3 == 180) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+if (angulo1 && angulo2 && angulo3 >= 0) {
+    console.log("angulos validos");
+} else {
+    console.log("Não form Triangulo");
+}
+
+//Exercicio 6
+
+//Exercicio 7
+
+let nota = 45;
+
+if (nota > 100) {
+    console.log("Nota Invalida");
+} else if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("E");
+} else if (nota <= 50) {
+    console.log("F");
+} 

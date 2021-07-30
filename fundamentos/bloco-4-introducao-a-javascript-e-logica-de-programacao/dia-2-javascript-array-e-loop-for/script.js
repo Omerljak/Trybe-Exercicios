58189c5ac9139b6e922b75ba0e9f65489a6c6000
Omar Merljak <omerljak@gmail.com>
2021-07-30 17:54:23 -0300

    resultados dos exercicios, apoio do gabarito...ainda com duvidas

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -40,7 +40,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 //Exercicio 4
 
-let soma = 0;
+/*let soma = 0;
 let media = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     soma = (soma + numbers[index]);
@@ -51,4 +51,62 @@ if (media > 20 ) {
 } else {
     console.log("valor menor ou igual a 20");
 }
+    resultadp "valor maior que 20"*/
+
+/*//Exercicio 5 DUVIDA PQ RESULTADO "5"
+
+let maiorNumero = numbers[0];
+for (let index = 1; index < numbers.legth; index += 1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
+}
+console.log(maiorNumero);*/
+
+
+//Exercicio 6 //   RESULTADO 6 NUMEROS IMPARES
+
+/*let resultado = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {  //pq !==
+        resultado += 1; //qual razâo/significado
+      }    
+    } 
+if (resultado === 0) {  //pq === e não apenas =
+    console.log("nenhum valor impar");
+} else {
+    console.log(resultado);
+}*/
     
+    
+
+//Exercicio 7 // DUVIDA resultado "5" porque
+
+/*let menorNumero = numbers[0];
+
+for (let index = 1; index < numbers.legth; index += 1) {
+    if (numbers[index] < menorNumero) {
+        menorNumero = numbers[index];
+    }
+}
+console.log(menorNumero);*/
+
+/*/Exercicio 8  //com ajuda do Gabarito//
+
+
+    let numbersNew = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbersNew.push(index);
+}
+
+console.log(numbersNew);*/
+
+
+//Exercicio 9
+
+
+for (let index = 1; index < numbers.length; index += 1) {
+  console.log(numbers[index] / 2);
+}
+

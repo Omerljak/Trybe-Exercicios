e2299a0339060532ce4afe542173d4b0ccbec70e
Omar Merljak <omerljak@gmail.com>
2021-08-02 10:40:30 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-e-programacao-e-algoritmos/script.js
@@ -0,0 +1,13 @@
+// EXERCICIOS 1
+
+let n = 5;
+let linha = "";
+let colun = "";
+
+
+for (let index = 0; index < n; index += 1) {
+    linha += "*";
+} 
+for (let index2 = 0; index2 < n; index2 += 1) {
+  console.log(linha);
+  }

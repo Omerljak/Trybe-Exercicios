2c0f1602c19f62bb42cc2e511821a09b4c67089c
Omar Merljak <omerljak@gmail.com>
2021-08-02 10:51:18 -0300

    exercicio 2 resolvido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-e-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-e-programacao-e-algoritmos/script.js
@@ -2,12 +2,19 @@
 
 let n = 5;
 let linha = "";
-let colun = "";
 
 
-for (let index = 0; index < n; index += 1) {
+
+/*for (let index = 0; index < n; index += 1) {
     linha += "*";
 } 
 for (let index2 = 0; index2 < n; index2 += 1) {
-  console.log(linha);
-  }
+    console.log(linha);
+  }*/
+
+  // EXERCICIOS 2
+
+  /*for (let index = 0; index < n; index += 1) {
+    linha += "*";
+      console.log(linha);
+ } */

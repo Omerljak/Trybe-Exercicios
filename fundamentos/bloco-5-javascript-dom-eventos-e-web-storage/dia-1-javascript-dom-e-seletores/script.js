ff9d5ac99fce5c5c8897c469c686f98712728cc9
Omar Merljak <omerljak@gmail.com>
2021-08-09 18:29:18 -0300

    exercicio de fixaçao ok

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,12 @@
+document.getElementById('header-container').style.backgroundColor= '#00cc99';
+
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'salmon';
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = 'purple';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = 'black';
+
+document.getElementById('footer-container').style.backgroundColor = '#004040';

616d06a3a7dffbb2138d2b6e230d8ecf36e77fcb
Omar Merljak <omerljak@gmail.com>
2021-08-12 08:53:29 -0300

    criado botaoexercicio 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/scriptCalendar.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/scriptCalendar.js
@@ -22,9 +22,42 @@ function createDaysOfTheWeek() {
       const daysMonth = dezDaysList[index];
       const dayMonthList = document.createElement('li');
       dayMonthList.innerHTML = daysMonth;
-  
       monthDaysList.appendChild(dayMonthList);
+      dayMonthList.classList.add('day');
+  /*if (daysMonth === 24 || daysMonth === 25 || daysMonth === 31) {
+      daysMonthList.classList.add('day');
+    } 
+    if (daysMonth === 4 || daysMonth === 11 || daysMonth === 18 || daysMonth === 25) {
+      daysMonthList.classList.add('holiday')
+    } */
     };
   };
   
   createDaysOfTheMonth();
+
+  //EXERCICIO 2
+  function createButton() {
+  let newButton = document.querySelector('.buttons-container');
+  let holiButton = document.createElement('button');
+    holiButton.innerHTML.id = 'btn-holiday';
+    holiButton.innerHTML = 'Feriados';
+    newButton.appendChild(holiButton);
+  }
+    createButton()
+
+ /*  //exercicio 3
+
+function holidaysButtonFunction() {
+  let holidaysButton = document.getElementById('btn-holiday');
+  holidaysButton.addEventListener('click', holidaysBgColorChanger());
+}
+
+function holidaysBgColorChanger() {
+  let holidays = document.getElementsByClassName('holiday');
+  console.log(holidays);
+  for (index = 0; index < holidays.length; index += 1) {
+    holidays[index].style.backgroundColor = 'blue';
+  }
+}
+
+holidaysButtonFunction() */

5bb8eb3e62ef0a789611374b8c9ebc6061c3549f
Omar Merljak <omerljak@gmail.com>
2021-08-12 14:35:30 -0300

    soluçao erro exercicio 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/scriptCalendar.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/scriptCalendar.js
@@ -13,61 +13,69 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Exercicio 1
+  // Exercicio 1 OK + nâo add 'holiday e friday'
   function createDaysOfTheMonth() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const monthDaysList = document.querySelector('#days');
   
-    for (let index = 0; index < dezDaysList.length; index += 1) {
-      const daysMonth = dezDaysList[index];
-      const dayMonthList = document.createElement('li');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+      const daysMonth = dezDaysList[index]; // onde estão os dias
+      const dayMonthList = document.createElement('li'); // onde criou-se as li
+     /*  dayMonthList.innerHTML = daysMonth;
+      monthDaysList.appendChild(dayMonthList);
+      dayMonthList.className = 'day'; */
+      //bloco transferido para dentro dos if's, assim fica mais simples a distribuição
+  if (daysMonth === 24 || daysMonth === 31) {
+      daysMonth.className = ' day holiday';
       dayMonthList.innerHTML = daysMonth;
       monthDaysList.appendChild(dayMonthList);
-      dayMonthList.classList.add('day');
-      
-  /* if (daysMonthList === 24 || daysMonthList === 25 || daysMonthList === 31) {
-      daysMonthList.classList.add('holiday');
-    } 
-    if (daysMonthList === 4 || daysMonthList === 11 || daysMonthList === 18 || daysMonthList === 25) {
-      daysMonthList.classList.add('friday')
-    } */
+    } else if (daysMonth === 4 || daysMonth === 11 || daysMonth === 18) {
+      daysMonth.className = 'day friday';
+      dayMonthList.innerHTML = daysMonth;
+      monthDaysList.appendChild(dayMonthList);
+    } else if (daysMonth === 25) {
+      daysMonth.className = 'day holiday, friday';
+      dayMonthList.innerHTML = daysMonth;
+      monthDaysList.appendChild(dayMonthList);
+    } else {
+      daysMonth.className = 'day';
+      dayMonthList.innerHTML = daysMonth;
+      monthDaysList.appendChild(dayMonthList);
     }
-  }
+    }
+    }
+    
+  
   createDaysOfTheMonth();
-
-  //EXERCICIO 2
+  
+  //EXERCICIO 2 OK
   function createButton() {
     let newButton = document.querySelector('.buttons-container');
     let holiButton = document.createElement('button');
-     holiButton.innerHTML.id = 'btn-holiday';
+     holiButton.id = 'btn-holiday';
      holiButton.innerHTML = 'Feriados';
      newButton.appendChild(holiButton);
   }
     createButton()
 
   //exercicio 3
-/* 
-function holidaysButtonFunction() {
-  let holidayButton = document.getElementById('btn-holiday');
-  holidayButton.addEventListener('click', holidayColorChanger());
-}
+//
 
-function holidaysBgColorChanger() {
-  let holidays = document.getElementsByClassName('holiday');
-  console.log('holidays');
-  for (index = 0; index < holidays.length; index += 1) {
-    holidays[index].style.backgroundColor = 'blue';
-  }
-} 
-
-holidaysButtonFunction()*/
-
-  //EXERCICIO 4
+  //EXERCICIO 4 OK
 function createButtonFriday() {
   let fridayButton = document.querySelector('.buttons-container');
-  let ButtonFriday = document.createElement('button');
-    ButtonFriday.innerHTML.id = 'btn-friday';
-    ButtonFriday.innerHTML = 'Sexta-Feira';
-    fridayButton.appendChild(ButtonFriday);
+  let buttonFriday = document.createElement('button');
+    buttonFriday.id = 'btn-friday';
+    buttonFriday.innerHTML = 'Sexta-Feira';
+    fridayButton.appendChild(buttonFriday);
   }
     createButtonFriday()
+
+    //EXERCICIO 5
+
+let clicButton = document.getElementById('btn-friday');
+    clicButton.addEventListener("click" ,onclick());
+function onclick() {
+  let mudaSexta = document.getElementById('friday');
+  mudaSexta = 'sextou';
+}
